69f8b690488071d509ef91d68798cc1b9532dc60
wangzu <wangzu@student.ethz.ch>
2020-06-25 12:01:54 +0200

    add multi embedding for lstm

--- lstm_model.py
+++ lstm_model.py
@@ -5,7 +5,8 @@ import torch.nn.functional as F
 class LstmModel(nn.Module):
     def __init__(self, n_classes, emb_sizes_list, word_rnn_size=None, word_rnn_layers=None, word_att_size=None, dropout=0.5, device=None):
         super().__init__()
-        self.lstm = nn.LSTM(emb_sizes_list[0], word_rnn_size, bidirectional=True)
+        emb_sum_sizes = sum(emb_sizes_list)
+        self.lstm = nn.LSTM(emb_sum_sizes, word_rnn_size, bidirectional=True)
         self.lin = nn.Linear(2*word_rnn_size, n_classes)
     
     def forward(self, embeddings):
